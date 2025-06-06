288724435c6a51afb4ef2213c118fd73ed25bdde
xenbu <linfaokianz@gmail.com>
2025-06-06 20:20:02 +0700

    Create rumah.js

---
+++ rumah.js
@@ -0,0 +1,10 @@
+<script type='text/javascript'>
+var bb855 = document.getElementsByClassName('sidebar-nav')[0];
+var html = "<li><a class='btn-menu' href='/' target='_blank'><i class='fa fa-sort-numeric-asc'></i> Prediksi Togel</a></li><li><a class='btn-menu' href='/' target='_blank'><i class='fa fa-rocket'></i>Live RTP Slot</a></li><li><a class='btn-menu' href='/' target='_blank'><i class='fa fa-money'></i> Bukti Pembayaran </a></li>";
+bb855.insertAdjacentHTML('beforeend',html);
+
+    document.addEventListener("DOMContentLoaded",function(){
+		$('<a title="Bandar Togel Terpercaya" target="_blank" href="/"><img src="" style="width:100%; margin-top: 15px;" /></ a>').insertAfter("#slider");
+
+	},false);
+</script>
